8f4143b427111177744fbc0c408bdb0dd7f4857f
thankal <thanasiskal81@gmail.com>
2022-12-29 14:33:57 +0200

    Initial Commit

---
+++ exercise1/network.java
@@ -0,0 +1,22 @@
+package exercise1;
+
+public class network {
+
+    private void feedforward() {
+        System.out.println("Feedforward");
+    }
+
+    private void backpropagation() {
+        System.out.println("Backpropagation");
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        System.out.println("Hello World");
+    }
+
+}
