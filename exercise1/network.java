38ebdcf3ac570d0306b9138c120de594651d43c4
thankal <thanasiskal81@gmail.com>
2022-12-31 19:16:04 +0200

    Implement feedforward function

--- exercise1/network.java
+++ exercise1/network.java
@@ -1,5 +1,6 @@
 package exercise1;
 import java.lang.Math;
+import java.util.ArrayList;
 
 public class network {
 
@@ -19,24 +20,142 @@ public class network {
     private int numOfH3Neurons;
 
     // Type of activation function
-    private int activationFunctionType;
+    private int ACTIVATION_FUNCTION_TYPE = 1;
 
     
-    // The inputs of the network
-    private double[][] inputs;
+    // The input vector
+    private ArrayList<Double> inputs = new ArrayList<Double>(d);
+
+    // The output vector
+    private ArrayList<Double> outputs = new ArrayList<Double>(K);
+
+
 
     // The weights of the network (for each of the 3 layers)
-    private double[][] weights;
+    private ArrayList<ArrayList<Double>> weights = new ArrayList<ArrayList<Double>>();
 
 
 
-    private void feedforward() {
-        System.out.println("Feedforward");
+
+
+
+
+
+    // initialize the weights with random values in the range [-1,1]
+    private void initRandomWeights() {
+        // add a random value for each weight of the first hidden layer
+        for (int i = 0; i < numOfH1Neurons; i++) {
+            ArrayList<Double> temp = new ArrayList<Double>(K);
+            for (int j = 0; j <= d; j++) {
+                // generate a random double between -1 and 1
+                temp.add((Math.random()*2-1));
+            }
+            weights.add(temp);
+        }
+
+        // add a random value for each weight of the second hidden layer
+        for (int i = 0; i < numOfH2Neurons; i++) {
+            ArrayList<Double> temp = new ArrayList<Double>(K);
+            for (int j = 0; j <= numOfH1Neurons; j++) {
+                temp.add((Math.random()*2-1));
+            }
+            weights.add(temp);
+        }
+
+        // add a random value for each weight of the third hidden layer
+        for (int i = 0; i < numOfH3Neurons; i++) {
+            ArrayList<Double> temp = new ArrayList<Double>(K);
+            for (int j = 0; j <= numOfH2Neurons; j++) {
+                temp.add((Math.random()*2-1));
+            }
+            weights.add(temp);
+        }
+
+        // add a random value for each weight of the output layer
+        for (int i = 0; i < K; i++) {
+            ArrayList<Double> temp = new ArrayList<Double>(K);
+            for (int j = 0; j <= numOfH3Neurons; j++) {
+                temp.add((Math.random()*2-1));
+            }
+            weights.add(temp);
+        }
+        
+    }
+
+    private ArrayList<Double> feedforward(ArrayList<Double> inputs, int d, int K) {
+        ArrayList<Double> outputs = new ArrayList<Double>(K);
+
+        // The activations of the neurons (for each of the 5 layers)
+        ArrayList<ArrayList<Double>> activations = new ArrayList<ArrayList<Double>>();
+
+        // add the inputs to the input layer activations
+        activations.add(inputs);
+
+        // calculate the activations of the first hidden layer
+        for (int i = 0; i < numOfH1Neurons; i++) {
+            // iterate through the inputs
+            for (int j = 1; j <= d; j++) {
+                // calculate the activation
+                double z = activations.get(0).get(j-1) * weights.get(0).get(j) + weights.get(0).get(0);
+                double new_activation = activationFunction(ACTIVATION_FUNCTION_TYPE, z);
+                activations.get(1).set(i, new_activation);
+            }
+        }
+
+        // calculate the activations of the second hidden layer
+        for (int i = 0; i < numOfH2Neurons; i++) {
+            // iterate through the inputs
+            for (int j = 1; j <= numOfH1Neurons; j++) {
+                // calculate the activation
+                double z = activations.get(1).get(j-1) * weights.get(1).get(j) + weights.get(1).get(0);
+                double new_activation = activationFunction(ACTIVATION_FUNCTION_TYPE, z);
+                activations.get(2).set(i, new_activation);
+            }
+        }
+
+        // calculate the activations of the third hidden layer
+        for (int i = 0; i < numOfH3Neurons; i++) {
+            // iterate through the inputs
+            for (int j = 1; j <= numOfH2Neurons; j++) {
+                // calculate the activation
+                double z = activations.get(2).get(j-1) * weights.get(2).get(j) + weights.get(2).get(0);
+                double new_activation = activationFunction(ACTIVATION_FUNCTION_TYPE, z);
+                activations.get(3).set(i, new_activation);
+            }
+        }
+
+        // calculate the activations of the output layer
+        for (int i = 0; i < K; i++) {
+            // iterate through the inputs
+            for (int j = 1; j <= numOfH3Neurons; j++) {
+                // calculate the activation
+                double z = activations.get(3).get(j-1) * weights.get(3).get(j) + weights.get(3).get(0);
+                double new_activation = activationFunction(ACTIVATION_FUNCTION_TYPE, z);
+                activations.get(4).set(i, new_activation);
+            }
+        }
+
+        outputs = activations.get(4);
+        return outputs;
     }
 
     private void backpropagation() {
         System.out.println("Backpropagation");
     }
+
+
+    // run the user specified activation function
+    private double activationFunction(int type, double x) {
+        switch (type) {
+            case 1:
+                return logistic(x);
+            case 2:
+                return tangent(x);
+            default:
+                return logistic(x);
+        }
+    }
+
 
     private double logistic(double x){
         return 1.0/(1+Math.exp(-x));
